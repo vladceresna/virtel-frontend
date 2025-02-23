8b690577c66adda20cc98797847994950a902288
Vlad Ĉereŝna <vladceresna6@gmail.com>
2025-02-23 11:51:35 +0200

    3.0.0 Maple

--- src/routes/download/+page.svelte
+++ src/routes/download/+page.svelte
@@ -17,7 +17,7 @@
   <h2
   class="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
   >
-   Cedrus 2.3.x Beta
+   Cedrus 2.3.0 Beta
   </h2>
   <h4 class="text-muted-foreground scroll-m-20 text-xl font-semibold tracking-tight">
       For every platform. Open Source. Your best virtual enviroment
@@ -50,7 +50,7 @@
 </div>
 <Alert.Root>
   <Terminal class="h-4 w-4" />
-  <Alert.Title>Virtel now is in Beta version</Alert.Title>
+  <Alert.Title>Virtel now is in Alpha version</Alert.Title>
   <Alert.Description>
     Please use this with caution. 
     This is OpenSource project with GNU GPLv3.0 License, 
@@ -59,6 +59,49 @@
     If you see bugs, feel free to contact me by email or create a problem on the github repository
   </Alert.Description>
 </Alert.Root>
+
+<div class="text-center gap-5 pt-12 flex flex-wrap flex-col">  
+  <h2
+  class="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+  >
+    Maple 3.0.0 Alpha
+  </h2>
+  <h4 class="text-muted-foreground scroll-m-20 text-xl font-semibold tracking-tight">
+      For every platform. Open Source. Your best virtual enviroment
+  </h4>
+</div>
+
+<div class="flex md:flex-row flex-col gap-5">
+    <DownloadOption 
+    url="https://github.com/vladceresna/virtel/releases/download/3.0.0-alpha/com.vladceresna.virtel-3.0.0-android.apk"
+    os="Android"
+    format="APK"
+    version="3.0.0"
+    weight="20 MB"
+    />
+    <DownloadOption 
+    url="https://github.com/vladceresna/virtel/releases/download/3.0.0-alpha/com.vladceresna.virtel-3.0.0-windows.exe"
+    os="Windows"
+    format="EXE"
+    version="3.0.0"
+    weight="80 MB"
+    />
+    <DownloadOption 
+    url="https://github.com/vladceresna/virtel/releases/download/3.0.0-alpha/com.vladceresna.virtel-3.0.0-debian.deb"
+    os="Debian/Ubuntu"
+    format="DEB"
+    version="3.0.0"
+    weight="121 MB"
+    />
+    <DownloadOption 
+    url="https://github.com/vladceresna/virtel/releases/download/3.0.0-alpha/com.vladceresna.virtel-3.0.0-linux.tar.xz"
+    os="Other Linux"
+    format="TAR.XZ"
+    version="3.0.0"
+    weight="124 MB"
+    />
+
+</div>
 <div class="text-center gap-5 flex flex-wrap flex-col">
   <h4 class="text-muted-foreground scroll-m-20 text-xl font-semibold tracking-tight">
    Previous Versions History.<br> Deprecated. Unsupported
