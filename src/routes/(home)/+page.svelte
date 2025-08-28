cb57c223c32b2f686976bd4c4d25605a94419fb7
Vlad Ĉereŝna <vladceresna6@gmail.com>
2025-08-28 15:13:33 +0300

    Refactor homepage content and layout

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -1,82 +1,137 @@
 <script lang="ts">
-    import { Button } from "$lib/components/ui/button";
-    import * as Card from '$lib/components/ui/card';
+	import CodeView from '$lib/components/code-view.svelte';
+	import Oh4 from '$lib/components/typography/oh4.svelte';
+	import { Button } from '$lib/components/ui/button';
+	import * as Card from '$lib/components/ui/card';
 </script>
 
-<div class="text-center grid gap-5 grid-cols-1 py-12 ">
-    <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-      Virtel Project.<br>
-      Operating System. Redefined
-    </h1>
-    <h4 class="text-muted-foreground scroll-m-20 text-xl font-semibold tracking-tight">
-        New reality. Speed. Secure. Cross-platform. Just. Open Source.
-        <br>
-        Your best virtual enviroment
-    </h4>
-    <div class="justify-center align-center">
-        <Button href="/docs" class="m-1">Get Started</Button>
-        <Button class="m-1" variant="secondary" href="https://discord.gg/hNSyTvuy2v">Join our Discord Community!</Button>
-    </div>
-    
+<div class="grid grid-cols-1 gap-5 py-12 text-center">
+	<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+		Virtel. Universal Runtime
+	</h1>
+	<h4
+		class="mx-auto max-w-4xl scroll-m-20 text-xl font-semibold tracking-tight text-muted-foreground"
+	>
+		Build full-fledged applications that run everywhere in a clean system.
+		<br />Work and websurf in clean and vibe ecosystem.
+	</h4>
+	<div class="align-center flex flex-wrap justify-center gap-2">
+		<Button href="/download">Download</Button>
+		<Button href="/docs" variant="secondary">Docs</Button>
+	</div>
+	<div class="mx-auto flex gap-2">
+		<Button class="my-auto font-bold" variant="outline" href="/about">Follow us</Button>
+		<Button variant="outline" size="icon" href="https://discord.gg/hNSyTvuy2v">
+			<img src="discord.svg" alt="Discord" class="h-4 w-4 dark:invert" />
+		</Button>
+		<Button variant="outline" size="icon" href="https://t.me/virtelx">
+			<img src="telegram.svg" alt="Telegram" class="h-4 w-4 dark:invert" />
+		</Button>
+		<Button variant="outline" size="icon" href="https://www.instagram.com/virtelx/">
+			<img src="instagram.svg" alt="Instagram" class="h-4 w-4 dark:invert" />
+		</Button>
+		<Button variant="outline" size="icon" href="https://www.threads.com/@virtelx">
+			<img src="threads.svg" alt="Threads" class="h-4 w-4 dark:invert" />
+		</Button>
+		<Button
+			variant="outline"
+			size="icon"
+			href="https://www.youtube.com/channel/UCbc-wBJEVRRMLENSaFlSaxg"
+		>
+			<img src="youtube.svg" alt="Youtube" class="h-4 w-4 dark:invert" />
+		</Button>
+	</div>
 </div>
-<iframe class="mx-10 rounded-2xl border md:h-[30rem]"
-src="https://www.youtube.com/embed/88I7QsRG0v8?si=t131GGXnhHGaqphF" 
-title="YouTube video player" frameborder="0" 
-allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+<iframe
+	class="mx-10 rounded-2xl border md:h-[30rem]"
+	src="https://www.youtube.com/embed/88I7QsRG0v8?si=t131GGXnhHGaqphF"
+	title="YouTube video player"
+	frameborder="0"
+	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+	referrerpolicy="strict-origin-when-cross-origin"
+	allowfullscreen
+>
 </iframe>
-<div class="flex md:flex-row flex-col justify-center align-middle place-content-center gap-10 p-10">
-    <Card.Root class="flex-1 w-full justify-center align-middle border rounded-lg">
-        <Card.Header>
-            <Card.Title class="text-3xl">Simplicity as you like!</Card.Title>
-            <Card.Description class="text-xl">It's so easy!</Card.Description>
-        </Card.Header>
-        <Card.Content>
-            <div class="my-5 whitespace-break-spaces rounded-2xl border bg-stone-200 px-3 py-2 font-mono dark:bg-stone-800">
-                csl write "Hello world!";
-            </div>
-        </Card.Content>
-        <Card.Footer>
-            <Button href="/docs/2.3.x/devdocs/steps">Learn more about Steps</Button>
-        </Card.Footer>
-    </Card.Root>
-    <div class="flex-1 w-full justify-center align-middle border rounded-lg">
-        <img src="steps-code.png" class="rounded-lg h-full w-full"/>
-    </div>
 
+<div class="flex flex-col place-content-center justify-center gap-10 p-10 align-middle md:flex-row">
+	<Card.Root class="w-full flex-1 justify-center rounded-lg border align-middle">
+		<Card.Header>
+			<Card.Title class="text-3xl">Simple. Expressive. Fast.</Card.Title>
+			<Card.Description class="text-xl"
+				>Build beautiful, complex UIs with code that reads like a sentence. Virtel combines a
+				declarative component system with a utility-first design philosophy.</Card.Description
+			>
+		</Card.Header>
+		<Card.Content>
+			<div>
+				- This is not just a website. It's a real app.<br />
+				- Every element is a native, high-performance component.<br />
+				<br />
+				<CodeView>csl write "Welcome to the next evolution of apps!";</CodeView>
+			</div>
+		</Card.Content>
+		<Card.Footer>
+			<Button href="/docs/3.x.x/devdocs/steps/screen">Explore the UI Framework</Button>
+		</Card.Footer>
+	</Card.Root>
+	<div
+		class="w-full flex-1 justify-center rounded-lg border bg-[#2A7B9B] bg-gradient-to-r from-[#2A7B9B] via-[#57C785] to-[#EDDD53] align-middle"
+	></div>
 </div>
-<div class="flex md:flex-row flex-col justify-center align-middle place-content-center gap-10 p-10">
-    <Card.Root class="flex-1 w-full justify-center align-middle border rounded-lg">
-        <Card.Header>
-            <Card.Title class="text-3xl">Ready parts of Steps code!</Card.Title>
-            <Card.Description class="text-xl">Learn Steps with Skills! Simple. Open Source.</Card.Description>
-        </Card.Header>
-        <Card.Footer>
-            <Button href="/skills">Learn more about Skills</Button>
-        </Card.Footer>
-    </Card.Root>
-    <div class="flex-1 w-full justify-center align-middle border rounded-lg">
-        <img src="skills-preview.png" class="rounded-lg h-full w-full"/>
-    </div>
-
+<div
+	class="flex flex-col place-content-center justify-center gap-10 p-10 align-middle md:flex-row-reverse"
+>
+	<Card.Root class="w-full flex-1 justify-center rounded-lg border align-middle">
+		<Card.Header>
+			<Card.Title class="text-3xl">One Platform. Any Application.</Card.Title>
+			<Card.Description class="text-xl"
+				>Stop choosing between a "website," a "desktop app," or a "game engine." Virtel's multi-API
+				architecture lets you build anything you can imagine.</Card.Description
+			>
+		</Card.Header>
+		<Card.Content></Card.Content>
+		<Card.Footer>
+			<Button href="/docs">Learn About the Architecture</Button>
+		</Card.Footer>
+	</Card.Root>
+	<div
+		class="w-full flex-1 justify-center rounded-lg border bg-[#833AB4] bg-gradient-to-r from-[#833AB4] via-[#FD1D1D] to-[#FCB045] align-middle"
+	></div>
+</div>
+<div class="flex flex-col place-content-center justify-center gap-10 p-10 align-middle md:flex-row">
+	<Card.Root class="w-full flex-1 justify-center rounded-lg border align-middle">
+		<Card.Header>
+			<Card.Title class="text-3xl">Get ready parts of Steps code!</Card.Title>
+			<Card.Description class="text-xl"
+				>Learn Steps with Skills! Open source components on our website</Card.Description
+			>
+		</Card.Header>
+		<Card.Footer>
+			<Button href="/skills">Learn more about Skills</Button>
+		</Card.Footer>
+	</Card.Root>
+	<div class="w-full flex-1 justify-center rounded-lg border align-middle">
+		<img alt="skills-preview" src="skills-preview.png" class="h-full w-full rounded-lg" />
+	</div>
 </div>
 <video controls autoplay name="media" class="mx-10 rounded-2xl border md:h-[30rem]">
-    <source src="/linux-virtel-preview.webm" type="video/webm">
+	<source src="/linux-virtel-preview.webm" type="video/webm" />
+	<track kind="captions" label="English" src="/english.vtt" srclang="en" default />
 </video>
-<div class="flex md:flex-row flex-col justify-center align-middle place-content-center gap-10 p-10">
-    <Card.Root class="flex-1 w-full justify-center align-middle border rounded-lg">
-        <Card.Header>
-            <Card.Title class="text-3xl">Safe execution enviroment!</Card.Title>
-            <Card.Description class="text-xl">Isolated. Open Source.<br>
-                Your powerful, safe and secure isolated crossplatform virtual environment
-            </Card.Description>
-        </Card.Header>
-        <Card.Footer>
-            <Button href="/docs">Learn more about Virtel</Button>
-        </Card.Footer>
-    </Card.Root>
-    <div class="flex-1 w-full justify-center align-middle border rounded-lg">
-        <img src="virtel-preview.png" class="rounded-lg h-full w-full"/>
-    </div>
-
+<div class="flex flex-col place-content-center justify-center gap-10 p-10 align-middle md:flex-row">
+	<Card.Root class="w-full flex-1 justify-center rounded-lg border align-middle">
+		<Card.Header>
+			<Card.Title class="text-3xl">Safe execution enviroment!</Card.Title>
+			<Card.Description class="text-xl"
+				>Isolated. Open Source.<br />
+				Your powerful, safe and secure isolated crossplatform virtual environment
+			</Card.Description>
+		</Card.Header>
+		<Card.Footer>
+			<Button href="/docs">Learn more about Virtel</Button>
+		</Card.Footer>
+	</Card.Root>
+	<div class="w-full flex-1 justify-center rounded-lg border align-middle">
+		<img alt="virtel-preview" src="virtel-preview.png" class="h-full w-full rounded-lg" />
+	</div>
 </div>
--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,17 +1,15 @@
 <script lang="ts">
-    import { Button } from "$lib/components/ui/button";
+	import { Button } from '$lib/components/ui/button';
 </script>
 
-
-<div align="center" class="grid gap-5 grid-cols-1 p-12">
-    <h1 class="scroll-m-20 text-8xl font-extrabold tracking-tight lg:text-10xl">
-      404
-    </h1>
-    <h4 class="text-muted-foreground scroll-m-20 text-2xl font-semibold tracking-tight">
-        Virtel Project
-    </h4>
-    <div class="justify-center">
-        <Button href="/" class="mr-2">Go to Home</Button>
-        <Button href="https://github.com/vladceresna/virtel/" variant="outline">Go to our Github</Button>
-    </div>
+<div align="center" class="grid grid-cols-1 gap-5 p-12">
+	<h1 class="lg:text-10xl scroll-m-20 text-8xl font-extrabold tracking-tight">404</h1>
+	<h4 class="scroll-m-20 text-2xl font-semibold tracking-tight text-muted-foreground">
+		Virtel. Universal Runtime
+	</h4>
+	<div class="justify-center">
+		<Button href="/" class="mr-2">Go to Home</Button>
+		<Button href="https://github.com/vladceresna/virtel/" variant="outline">Go to our Github</Button
+		>
+	</div>
 </div>
