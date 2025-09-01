bbf81709a92cb2e0c3291221769684987ad8809f
Vlad Ĉereŝna <vladceresna6@gmail.com>
2025-09-01 22:24:52 +0300

    To 3.4.0

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,126 +1,119 @@
+<script lang="ts">
+	import '../app.css';
+	import Activity from 'lucide-svelte/icons/activity';
+	import ArrowUpRight from 'lucide-svelte/icons/arrow-up-right';
+	import CircleUser from 'lucide-svelte/icons/circle-user';
+	import CreditCard from 'lucide-svelte/icons/credit-card';
+	import DollarSign from 'lucide-svelte/icons/dollar-sign';
+	import Menu from 'lucide-svelte/icons/menu';
+	import Package2 from 'lucide-svelte/icons/package-2';
+	import Search from 'lucide-svelte/icons/search';
+	import Users from 'lucide-svelte/icons/users';
+
+	import * as Avatar from '$lib/components/ui/avatar/index.js';
+	import { Badge } from '$lib/components/ui/badge/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import * as Card from '$lib/components/ui/card/index.js';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import * as Sheet from '$lib/components/ui/sheet/index.js';
+	import * as Table from '$lib/components/ui/table/index.js';
+	import { ModeWatcher } from 'mode-watcher';
+	import ModeToggle from '$lib/components/mode-toggle.svelte';
+	import { Toaster } from '$lib/components/ui/sonner';
+
+	let searchQuery = '';
+</script>
+
 <svelte:head>
-	<title>The best system for operating your device | Virtel Project | Full Control</title>
-	<meta name="description" content="Powerful, user-friendly and super-lightweight operating system - the way to new generation of Web 3.0 Internet. Virtualize your world! Download Virtel!">
+	<title>Virtel | Universal Runtime | Your apps running truly natively everywhere</title>
+	<meta
+		name="description"
+		content="Truly crossplatform and open source universal runtime environment - the way to new generation of Web 3.0 Internet. Virtualize your world! Download Virtel!"
+	/>
 	<meta name="google-site-verification" content="E6ni2ZbK0IHj84kPBZ7Tw7_9iZn_QbpW7TNQMa8IPX0" />
 </svelte:head>
 
-<script lang="ts">
-	import '../app.css';
-	import Activity from "lucide-svelte/icons/activity";
-	import ArrowUpRight from "lucide-svelte/icons/arrow-up-right";
-	import CircleUser from "lucide-svelte/icons/circle-user";
-	import CreditCard from "lucide-svelte/icons/credit-card";
-	import DollarSign from "lucide-svelte/icons/dollar-sign";
-	import Menu from "lucide-svelte/icons/menu";
-	import Package2 from "lucide-svelte/icons/package-2";
-	import Search from "lucide-svelte/icons/search";
-	import Users from "lucide-svelte/icons/users";
-  
-	import * as Avatar from "$lib/components/ui/avatar/index.js";
-	import { Badge } from "$lib/components/ui/badge/index.js";
-	import { Button } from "$lib/components/ui/button/index.js";
-	import * as Card from "$lib/components/ui/card/index.js";
-	import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
-	import { Input } from "$lib/components/ui/input/index.js";
-	import * as Sheet from "$lib/components/ui/sheet/index.js";
-	import * as Table from "$lib/components/ui/table/index.js";
-	import { ModeWatcher } from "mode-watcher";
-	import ModeToggle from "$lib/components/mode-toggle.svelte";
-	import { Toaster } from "$lib/components/ui/sonner";
+<ModeWatcher />
+<Toaster />
 
-	let searchQuery = "";
-
-</script>
-
-  <ModeWatcher />
-  <Toaster />
-
-  <div class="font-inter flex min-h-screen w-full flex-col">
-	<div class="relative justify-center bg-primary p-2 flex items-center border-b gap-1 px-4 md:px-6 bg-primary supports-[backdrop-filter]:bg-primary backdrop-blur flex md:flex-row flex-col"
+<div class="flex min-h-screen w-full flex-col font-inter">
+	<div
+		class="relative flex flex flex-col items-center justify-center gap-1 border-b bg-primary bg-primary p-2 px-4 backdrop-blur supports-[backdrop-filter]:bg-primary md:flex-row md:px-6"
 	>
 		<h4 class="leading-0 font-bold text-white">
-			Meet Virtel Maple 3.3.x! Packs, UI, PQCrypto and Fixes.
+			Meet Virtel Maple 3.4.x! New UI, Custom colors, PQCrypto and Fixes.
 		</h4>
-		<Button href="/download" variant="outline" class="p-3 h-7 rounded-sm">
-			Download
-		</Button>
+		<Button href="/download" variant="outline" class="h-7 rounded-sm p-3">Download</Button>
 	</div>
 	<div class="sticky top-0 z-50">
-	
-	
-	
-	
-
-
-
-	<header class="bg-background sticky flex h-16 items-center gap-4 border-b px-4 md:px-6 bg-background/95 supports-[backdrop-filter]:bg-background/60 backdrop-blur">
-	  <nav
-		class="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6"
-	  >
-		<a href="/" class="notranslate flex items-center gap-2 text-lg font-semibold md:text-base mr-10">
-		  <img src="/favicon.png" alt="Logo" class="h-8 w-8" />
-		  <span>Virtel</span>
-		</a>
-		<a href="/" class="text-muted-foreground hover:text-foreground transition-colors">
-		  Home
-		</a>
-		<a href="/download" class="text-muted-foreground hover:text-foreground transition-colors">
-		  Download
-		</a>
-		<a href="/docs" class="text-muted-foreground hover:text-foreground transition-colors">
-		  Docs
-		</a>
-		<a href="/skills" class="text-muted-foreground hover:text-foreground transition-colors">
-		  Skills
-		</a>
-		<a href="/about" class="text-muted-foreground hover:text-foreground transition-colors">
-		  About
-		</a>
-	  </nav>
-	  <Sheet.Root>
-		<Sheet.Trigger asChild let:builder>
-		  <Button
-			variant="outline"
-			size="icon"
-			class="shrink-0 md:hidden"
-			builders={[builder]}
-		  >
-			<Menu class="h-5 w-5" />
-			<span class="sr-only">Toggle navigation menu</span>
-		  </Button>
-		</Sheet.Trigger>
-		<Sheet.Content side="left">
-		  <nav class="grid gap-6 text-lg font-medium">
-			<a href="##" class="notranslate flex items-center gap-2 text-lg font-semibold">
-				<img src="/favicon.png" alt="Virtel Logo" class="h-8 w-8" />
-				<span>Virtel</span>
-			</a>
-			<a href="/" class="text-muted-foreground hover:text-foreground"> Home </a>
-			<a href="/download" class="text-muted-foreground hover:text-foreground"> Download </a>
-			<a href="/docs" class="text-muted-foreground hover:text-foreground"> Docs </a>
-			<a href="/skills" class="text-muted-foreground hover:text-foreground"> Skills </a>
-			<a href="/about" class="text-muted-foreground hover:text-foreground"> About </a>
-		  </nav>
-		</Sheet.Content>
-	  </Sheet.Root>
-	  <div class="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-		<form class="ml-auto flex-1 sm:flex-initial">
-		  <div class="relative">
-			<Search class="text-muted-foreground absolute left-2.5 top-2.5 h-4 w-4" />
-			<Input
-			  type="search"
-			  placeholder="Search skills..."
-			  class="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
-			  bind:value={searchQuery} on:keydown={(e) => {
-				if (e.key === 'Enter') {
-					javascript:location.href = '/skills/search/'+searchQuery;
-				}
-			  }}
-			/>
-		  </div>
-		</form>
-		<ModeToggle/>
-		<!--
+		<header
+			class="sticky flex h-16 items-center gap-4 border-b bg-background bg-background/95 px-4 backdrop-blur supports-[backdrop-filter]:bg-background/60 md:px-6"
+		>
+			<nav
+				class="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6"
+			>
+				<a
+					href="/"
+					class="notranslate mr-10 flex items-center gap-2 text-lg font-semibold md:text-base"
+				>
+					<img src="/favicon.png" alt="Logo" class="h-8 w-8" />
+					<span>Virtel</span>
+				</a>
+				<a href="/" class="text-muted-foreground transition-colors hover:text-foreground"> Home </a>
+				<a href="/download" class="text-muted-foreground transition-colors hover:text-foreground">
+					Download
+				</a>
+				<a href="/docs" class="text-muted-foreground transition-colors hover:text-foreground">
+					Docs
+				</a>
+				<a href="/skills" class="text-muted-foreground transition-colors hover:text-foreground">
+					Skills
+				</a>
+				<a href="/about" class="text-muted-foreground transition-colors hover:text-foreground">
+					About
+				</a>
+			</nav>
+			<Sheet.Root>
+				<Sheet.Trigger asChild let:builder>
+					<Button variant="outline" size="icon" class="shrink-0 md:hidden" builders={[builder]}>
+						<Menu class="h-5 w-5" />
+						<span class="sr-only">Toggle navigation menu</span>
+					</Button>
+				</Sheet.Trigger>
+				<Sheet.Content side="left">
+					<nav class="grid gap-6 text-lg font-medium">
+						<a href="##" class="notranslate flex items-center gap-2 text-lg font-semibold">
+							<img src="/favicon.png" alt="Virtel Logo" class="h-8 w-8" />
+							<span>Virtel</span>
+						</a>
+						<a href="/" class="text-muted-foreground hover:text-foreground"> Home </a>
+						<a href="/download" class="text-muted-foreground hover:text-foreground"> Download </a>
+						<a href="/docs" class="text-muted-foreground hover:text-foreground"> Docs </a>
+						<a href="/skills" class="text-muted-foreground hover:text-foreground"> Skills </a>
+						<a href="/about" class="text-muted-foreground hover:text-foreground"> About </a>
+					</nav>
+				</Sheet.Content>
+			</Sheet.Root>
+			<div class="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+				<form class="ml-auto flex-1 sm:flex-initial">
+					<div class="relative">
+						<Search class="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+						<Input
+							type="search"
+							placeholder="Search skills..."
+							class="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
+							bind:value={searchQuery}
+							on:keydown={(e) => {
+								if (e.key === 'Enter') {
+									javascript: location.href = '/skills/search/' + searchQuery;
+								}
+							}}
+						/>
+					</div>
+				</form>
+				<ModeToggle />
+				<!--
 		<DropdownMenu.Root>
 		  <DropdownMenu.Trigger asChild let:builder>
 			<Button
@@ -143,18 +136,18 @@
 		  </DropdownMenu.Content>
 		</DropdownMenu.Root>
 		-->
-	  </div>
-	</header>
+			</div>
+		</header>
 	</div>
-	
+
 	<main class="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
-		<slot/>
+		<slot />
 	</main>
 	<footer class="p-10">
-		<p class="text-muted-foreground text-sm">
-			Built & designed by <a class="border-b" href="https://vladceresna.netlify.app/">vladceresna</a>. 
-			The source code is available on <a class="border-b" href="https://github.com/vladceresna/virtel">GitHub</a>.
+		<p class="text-sm text-muted-foreground">
+			Built & designed by <a class="border-b" href="https://vladceresna.netlify.app/">vladceresna</a
+			>. The source code is available on
+			<a class="border-b" href="https://github.com/vladceresna/virtel">GitHub</a>.
 		</p>
 	</footer>
-  </div>
-  
+</div>
--- src/routes/download/+page.svelte
+++ src/routes/download/+page.svelte
@@ -1,115 +1,102 @@
 <script lang="ts">
-  import { Button } from "$lib/components/ui/button";
-  import Terminal from "lucide-svelte/icons/terminal";
-  import * as Alert from "$lib/components/ui/alert/index.js";
-  import GitCommitHorizontal from "lucide-svelte/icons/git-commit-horizontal";
-  import MemoryStick from "lucide-svelte/icons/memory-stick";
-  import * as Avatar from "$lib/components/ui/avatar/index.js";
-  import * as HoverCard from "$lib/components/ui/hover-card/index.js";
-	import DownloadOption from "$lib/components/download-option.svelte";
+	import { Button } from '$lib/components/ui/button';
+	import Terminal from 'lucide-svelte/icons/terminal';
+	import * as Alert from '$lib/components/ui/alert/index.js';
+	import GitCommitHorizontal from 'lucide-svelte/icons/git-commit-horizontal';
+	import MemoryStick from 'lucide-svelte/icons/memory-stick';
+	import * as Avatar from '$lib/components/ui/avatar/index.js';
+	import * as HoverCard from '$lib/components/ui/hover-card/index.js';
+	import DownloadOption from '$lib/components/download-option.svelte';
 </script>
 
-<div class="text-center gap-5 pt-12 flex flex-wrap flex-col">
-  <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-    Download Virtel
-  </h1>
-      
-  <h2
-  class="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
-  >
-   Maple 3.3.0 Beta
-  </h2>
-  <h4 class="text-muted-foreground scroll-m-20 text-xl font-semibold tracking-tight">
-      For every platform. Open Source. Your best virtual enviroment
-  </h4>
+<div class="flex flex-col flex-wrap gap-5 pt-12 text-center">
+	<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Download Virtel</h1>
+
+	<h2 class="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+		Maple 3.4.0 Beta
+	</h2>
+	<h4 class="scroll-m-20 text-xl font-semibold tracking-tight text-muted-foreground">
+		Virtel is crossplatform, so you can use apps, built for Virtel everywhere.
+		<br />
+		Truly crossplatform and open source universal runtime.
+	</h4>
 </div>
 
-<div class="flex md:flex-row flex-col gap-5">
-  <DownloadOption 
-  url="https://github.com/vladceresna/virtel/releases/download/3.3.0-beta/com.vladceresna.virtel-3.3.0-android.apk"
-  os="Android
-  (Android 14 is not supported now)"
-  format="APK"
-  version="3.3.0"
-  weight="21 MB"
-  />
-  <DownloadOption 
-  url="https://github.com/vladceresna/virtel/releases/download/3.3.0-beta/com.vladceresna.virtel-3.3.0-windows.exe"
-  os="Windows"
-  format="EXE"
-  version="3.3.0"
-  weight="80 MB"
-  />
-  <DownloadOption 
-  url="https://github.com/vladceresna/virtel/releases/download/3.3.0-beta/com.vladceresna.virtel-3.3.0-debian.deb"
-  os="Debian/Ubuntu"
-  format="DEB"
-  version="3.3.0"
-  weight="80 MB"
-  />
-  <DownloadOption 
-  url="https://github.com/vladceresna/virtel/releases/download/3.3.0-beta/com.vladceresna.virtel-3.3.0-linux.tar.xz"
-  os="Other Linux"
-  format="TAR.XZ"
-  version="3.3.0"
-  weight="80 MB"
-  />
+<div class="flex flex-col gap-5 md:flex-row">
+	<DownloadOption
+		url="https://github.com/vladceresna/virtel/releases/download/3.4.0-beta/com.vladceresna.virtel-3.4.0-android.apk"
+		os="Android"
+		format="APK"
+		version="3.4.0"
+		weight="34 MB"
+	/>
+	<DownloadOption
+		url="https://github.com/vladceresna/virtel/releases/download/3.4.0-beta/com.vladceresna.virtel-3.4.0-windows.exe"
+		os="Windows"
+		format="EXE"
+		version="3.4.0"
+		weight="77 MB"
+	/>
+	<DownloadOption
+		url="https://github.com/vladceresna/virtel/releases/download/3.4.0-beta/com.vladceresna.virtel-3.4.0-linux.tar.xz"
+		os="Linux"
+		format="TAR.XZ"
+		version="3.4.0"
+		weight="78 MB"
+	/>
 </div>
 
 <Alert.Root>
-  <Terminal class="h-4 w-4" />
-  <Alert.Title>Virtel now is in Alpha version</Alert.Title>
-  <Alert.Description>
-    Please use this with caution. 
-    This is OpenSource project with GNU GPLv3.0 License, 
-    and we don&apos;t offer any guaranties of normal work this software. 
-    But you are welcome to test Virtel. 
-    If you see bugs, feel free to contact me by email or create a problem on the github repository
-  </Alert.Description>
+	<Terminal class="h-4 w-4" />
+	<Alert.Title>Virtel now is in Beta version</Alert.Title>
+	<Alert.Description>
+		Please use this with caution. This is OpenSource project with GNU GPLv3.0 License, and we
+		don&apos;t offer any guaranties of normal work this software. But you are welcome to test
+		Virtel. If you see bugs, feel free to contact me by email or create a problem on the github
+		repository
+	</Alert.Description>
 </Alert.Root>
 
-<div class="text-center gap-5 pt-12 flex flex-wrap flex-col">  
-  <h2
-  class="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
-  >
-    Cedrus 2.3.0 Beta (Unsupported)
-  </h2>
-  <h4 class="text-muted-foreground scroll-m-20 text-xl font-semibold tracking-tight">
-      For every platform. Open Source. Your best virtual enviroment
-  </h4>
+<div class="flex flex-col flex-wrap gap-5 pt-12 text-center">
+	<h2 class="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+		Cedrus 2.3.0 Beta (Unsupported)
+	</h2>
+	<h4 class="scroll-m-20 text-xl font-semibold tracking-tight text-muted-foreground">
+		For every platform. Open Source. Your best virtual enviroment
+	</h4>
 </div>
 
-<div class="flex md:flex-row flex-col gap-5">
-  <DownloadOption 
-  url="https://github.com/vladceresna/virtel/releases/download/2.3.0-beta/com.vladceresna.virtel-2.3.0-windows.exe"
-  os="Windows"
-  format="EXE"
-  version="2.3.0"
-  weight="80.3 MB"
-  />
-  <DownloadOption 
-  url="https://github.com/vladceresna/virtel/releases/download/2.3.0-beta/com.vladceresna.virtel-2.3.0-linux.deb"
-  os="Linux"
-  format="DEB"
-  version="2.3.0"
-  weight="121 MB"
-  />
-  <DownloadOption 
-  url="https://github.com/vladceresna/virtel/releases/download/2.3.0-beta/com.vladceresna.virtel-2.3.0-android.apk"
-  os="Android"
-  format="APK"
-  version="2.3.0"
-  weight="19 MB"
-  />
+<div class="flex flex-col gap-5 md:flex-row">
+	<DownloadOption
+		url="https://github.com/vladceresna/virtel/releases/download/2.3.0-beta/com.vladceresna.virtel-2.3.0-windows.exe"
+		os="Windows"
+		format="EXE"
+		version="2.3.0"
+		weight="80.3 MB"
+	/>
+	<DownloadOption
+		url="https://github.com/vladceresna/virtel/releases/download/2.3.0-beta/com.vladceresna.virtel-2.3.0-linux.deb"
+		os="Linux"
+		format="DEB"
+		version="2.3.0"
+		weight="121 MB"
+	/>
+	<DownloadOption
+		url="https://github.com/vladceresna/virtel/releases/download/2.3.0-beta/com.vladceresna.virtel-2.3.0-android.apk"
+		os="Android"
+		format="APK"
+		version="2.3.0"
+		weight="19 MB"
+	/>
 </div>
 
-<div class="text-center gap-5 flex flex-wrap flex-col">
-  <h4 class="text-muted-foreground scroll-m-20 text-xl font-semibold tracking-tight">
-   Previous Versions History.<br> Deprecated. Unsupported
-  </h4>
-  <div>
-    <Button href="https://github.com/vladceresna/virtel/releases" variant="outline" class="m-1">
-      See releases
-    </Button>
-  </div>
+<div class="flex flex-col flex-wrap gap-5 text-center">
+	<h4 class="scroll-m-20 text-xl font-semibold tracking-tight text-muted-foreground">
+		Previous Versions History.<br /> Deprecated. Unsupported
+	</h4>
+	<div>
+		<Button href="https://github.com/vladceresna/virtel/releases" variant="outline" class="m-1">
+			See releases
+		</Button>
+	</div>
 </div>
